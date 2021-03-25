70ccc5270cea9125f2119cebc5e5e0908af2f5e6
Elvis Baranoski <elvisbaranoski@gmail.com>
2021-03-25 07:50:33 -0300

    Criado Multi-Module Maven Projeto: KompraokProject, KompraokCommon,KompraokWebParent,KompraokBackEnd e KompraokFrontEnd

---
+++ KompraokWebParent/KompraokBackEnd/src/main/java/com/kompraok/admin/KompraokBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.kompraok.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KompraokBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KompraokBackEndApplication.class, args);
+	}
+
+}
